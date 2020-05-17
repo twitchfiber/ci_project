c2ad2b74ab58d2356f3097b980965458500fafcf
twitchfiber <13280148+twitchfiber@users.noreply.github.com>
2020-05-17 16:42:29 -0700

    UNIX Time Function 2 Converter (#3)
    * leapyear func
    
    * Made my_datetime return baseline epoch with time of 0
    
    * Wrote test to check for time > 0 since epoch
    
    * Make my_datetime correctly return date given valid unix sec time
    
    * Wrote more unit tests for my_datetime to validate
    
    * linting fixes whitespace

---
+++ task.py
@@ -0,0 +1,57 @@
+import math
+
+
+def my_datetime(num_sec):
+    months = ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10',
+              '11', '12']
+
+    # Leap year: 29, Non-Leap: 28
+    month_days = {
+        '01': 31,
+        '02': 28,
+        '03': 31,
+        '04': 30,
+        '05': 31,
+        '06': 30,
+        '07': 31,
+        '08': 31,
+        '09': 30,
+        '10': 31,
+        '11': 30,
+        '12': 31
+    }
+
+    if num_sec == 0:
+        return '01-01-1970'
+
+    num_days = num_sec / 86400.0
+
+    current_year = 1970
+    while num_days >= 365:
+        if is_leapyear(current_year):
+            num_days -= 366
+        else:
+            num_days -= 365
+        current_year += 1
+
+    i = 0
+    current_year_is_leap = is_leapyear(current_year)
+    while num_days >= month_days[months[i]]:
+        if i == 1 and current_year_is_leap:
+            num_days -= 29
+        else:
+            num_days -= month_days[months[i]]
+        i += 1
+
+    return '-'.join([str(months[i]), str(math.ceil(num_days)),
+                     str(current_year)])
+
+
+# Reference: https://en.wikipedia.org/wiki/Leap_year
+def is_leapyear(year):
+    if type(year) is not int:
+        return False
+    if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
+        return True
+    else:
+        return False
--- tests.py
+++ tests.py
@@ -1,10 +1,63 @@
 import unittest
+from task import is_leapyear, my_datetime
 
 
 class TestCase(unittest.TestCase):
 
-    def test1(self):
-        self.assertTrue(True)
+    def test_time1(self):
+        time = 0
+        expected = '01-01-1970'
+        self.assertEqual(my_datetime(time), expected)
+
+    def test_time2(self):
+        time = 123456789
+        expected = '11-29-1973'
+        self.assertEqual(my_datetime(time), expected)
+
+    def test_time3(self):
+        time = 9876543210
+        expected = '12-22-2282'
+        self.assertEqual(my_datetime(time), expected)
+
+    def test_time4(self):
+        time = 1589429599
+        expected = '05-14-2020'
+        self.assertEqual(my_datetime(time), expected)
+
+    def test_time5(self):
+        time = 1232353451
+        expected = '01-19-2009'
+        self.assertEqual(my_datetime(time), expected)
+
+    def test_leap1(self):
+        year = 1972
+        expected = True
+        self.assertEqual(is_leapyear(year), expected)
+
+    def test_leap2(self):
+        year = 1970
+        expected = False
+        self.assertEqual(is_leapyear(year), expected)
+
+    def test_leap3(self):
+        year = 2020
+        expected = True
+        self.assertEqual(is_leapyear(year), expected)
+
+    def test_leap4(self):
+        year = 2022
+        expected = False
+        self.assertEqual(is_leapyear(year), expected)
+
+    def test_leap5(self):
+        year = None
+        expected = False
+        self.assertEqual(is_leapyear(year), expected)
+
+    def test_leap6(self):
+        year = ''
+        expected = False
+        self.assertEqual(is_leapyear(year), expected)
 
 
 if __name__ == '__main__':
